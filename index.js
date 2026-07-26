879a601a69000bb763e56596676c5da48c528787
Матвей Безводинских <matvejbezvodinskih642@gmail.com>
2026-07-26 23:41:45 +0700

    Add JS stack practice with push/pop example

---
+++ index.js
@@ -0,0 +1,10 @@
+let stack = []
+stack.push ('действие 1')
+stack.push ('действие 2')
+stack.push ('действие 3')
+
+console.log (stack)
+
+let lastAction = stack.pop()
+console.log = (lastAction)
+console.log (stack)
